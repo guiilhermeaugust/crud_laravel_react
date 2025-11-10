e96a0ca7272df798b50825c52a04a0b363b97399
Guilherme Augusto Mendes <153883578+guiilhermeaugust@users.noreply.github.com>
2025-11-10 09:52:28 -0300

    UpdateNavBar
    Removed logo display from the Navbar component.

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,17 +1,10 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // Opcional: para estilização
-import Logo from "../../assets/images/logo.png";
 
 function Navbar() {
   return (
     <nav className="menu">
-      <div className="logo">
-        <Link to="/">
-          <img src={Logo} alt="Eduteka" title="Eduteka" />
-        </Link>
-      </div>
-
-      <ul>
+        <ul>
         <li>
           <Link to="/">Listagem</Link>
         </li>
